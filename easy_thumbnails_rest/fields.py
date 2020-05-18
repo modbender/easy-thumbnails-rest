4b7a22fb63450868b5ef5efdee2c0ad92a7bbd50
yashas123 <rameshmamathayashas@gmail.com>
2020-05-19 01:45:09 +0530

    Initial release

---
+++ easy_thumbnails_rest/fields.py
@@ -0,0 +1,37 @@
+from django.conf import settings
+from rest_framework.serializers import ImageField, JSONField
+from easy_thumbnails.files import get_thumbnailer
+
+__all__ = ('ThumbnailerField', 'ThumbnailerListField')
+
+THUMBNAIL_ALIASES = getattr(settings, 'THUMBNAIL_ALIASES', {})
+
+def image_sizes(request, image, alias_obj, alias_key):
+    if alias_key not in alias_obj:
+        raise KeyError('Key %s not found in dict thumbnail aliases'%alias_key)
+    i_sizes = alias_obj[alias_key].keys()
+    return {
+        **{k: request.build_absolute_uri(image[k].url) for k in i_sizes},
+        'original': request.build_absolute_uri(image.url)
+    }
+
+class ThumbnailerField(ImageField):
+    def __init__(self, **kwargs):
+        self.alias = kwargs.pop('alias', '')
+        super(ThumbnailerField, self).__init__(**kwargs)
+
+    def to_representation(self, instance):
+        if self.alias:
+            return self.context['request'].build_absolute_uri(instance[self.alias].url)
+        return super().to_representation(instance)
+
+class ThumbnailerListField(JSONField):
+    def __init__(self, **kwargs):
+        self.alias = kwargs.pop('alias', None)
+        self.alias_obj = kwargs.pop('alias_obj', THUMBNAIL_ALIASES)
+        super(ThumbnailerListField, self).__init__(**kwargs)
+
+    def to_representation(self, instance):
+        if self.alias or self.alias == '':
+            return image_sizes(self.context['request'], instance, self.alias_obj, self.alias)
+        return {}
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+setuptools.setup(
+    name='easy_thumbnails_rest',
+    version='1.0',
+    url='https://github.com/yashas123/easy-thumbnails-rest',
+    description='Easy Thumbnails Fields for Django Rest API',
+    long_description='Easy Thumbnails Fields for Django Rest API Framework',
+    author='Yashas H R',
+    author_email='rameshmamathayashas@gmail.com',
+    install_requires=[
+        'django',
+        'djangorestframework',
+        'easy-thumbnails',
+    ],
+    python_requires='>=3.5',
+    platforms=['any'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
